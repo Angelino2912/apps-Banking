cf2745b96b5abd190f2e5df3df557d8871639f45
zakialfarisi1234-hue <zakialfarisi1234@gmail.com>
2025-11-27 10:41:32 +0700

    push lek

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 import datauser as us
+from tkinter import messagebox
 
 user = us.DataUser("Dosen", "dosen123", "0", "admin")
 
@@ -16,12 +17,12 @@ class MainApp:
         self.title = tk.Label(self.root, text="Hallo Kamu!!", font=("Segoe UI", 15, "bold"), bg="#ffffff")
         self.title.pack(pady=(20, 20))
 
-        self.label = ttk.Label(root, text="Username",background="#fd83f1", font=("Comic Sans MS", 13, "bold"))
+        self.label = ttk.Label(root, text="Username",background="#83f9fd", font=("Comic Sans MS", 13, "bold"))
         self.label.pack()
         self.entry_username = ttk.Entry(self.root, width=30)
         self.entry_username.pack(pady=(0,10))
 
-        self.label = ttk.Label(root, text="Passsword",background="#fd83f1", font=("Comic Sans MS", 13, "bold") )
+        self.label = ttk.Label(root, text="Passsword",background="#83d4fd", font=("Comic Sans MS", 13, "bold") )
         self.label.pack()
         self.entry_password = ttk.Entry(self.root,show="*", width=30)
         self.entry_password.pack(pady=(0,20))
@@ -38,15 +39,30 @@ class MainApp:
         print("Password:", password)
         datauser =  user.panggil_user(username,password)
         print(datauser)
+        self.admin_login()
 
         if len(datauser):
-            print("login berhasil")
-            print(f"anda login sebagai {datauser[5]}")
+            messagebox.showinfo("Success", f"Welcome {datauser[5]}!")
+            self.admin_login()
+            
+        else :
+            messagebox.showerror("Failed", "Invalid username or password")
              
-        else:
-            print("Username or Password salah")
+
+    def admin_login(self):
+        self.clear_window()
+        tk.Label(self.root, text="MENU ADMIN BANK", font=("Arial", 16)).pack(pady=20)
+
+        tk.Button(self.root, text="Lihat Nasabah", width=25).pack(pady=5)
+        tk.Button(self.root, text="Tambah Nasabah", width=25).pack(pady=5)
+        tk.Button(self.root, text="Blokir Nasabah", width=25).pack(pady=5)
+        tk.Button(self.root, text="Lihat Transaksi", width=25).pack(pady=5)
+        tk.Button(self.root, text="Keluar", width=25, command=root.quit).pack(pady=20)
 
 
+    def clear_window(self):
+	    for widget in self.root.winfo_children():
+	        widget.destroy()
 root = Tk()
 app = MainApp(root)
 root.mainloop()

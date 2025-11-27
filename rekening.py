829a2cefee293840bec4ff47d41b37893ca417a6
Angelinooo <alvinjr2912@gmail.com>
2025-11-27 11:52:48 +0700

    comitt lek

--- rekening.py
+++ rekening.py
@@ -1,10 +1,21 @@
-import database
+import database   
 
 class Rekening:
-    def __init__(self, nomor_rekening, username, saldo=0):
+    def __init__(self,nomor_rekening):
         self.nomor_rekening = nomor_rekening
-        self.username = username
-        self.saldo = saldo
+
+        self.conn = database.db.mydb
+        self.cursor = self.conn.cursor()
+
+        query = "SELECT username, saldo FROM rekening WHERE nomor_rekening = %s"
+        self.cursor.execute(query, (self.nomor_rekening,))
+        result = self.cursor.fetchone()
+
+        if result:
+            self.username = result[0]
+            self.saldo = result[1]
+        else:
+            raise Exception("Nomor rekening tidak ditemukan!")
 
     def cek_saldo(self):
         print(f"Saldo Anda: Rp {self.saldo}")
@@ -12,7 +23,13 @@ class Rekening:
     def setor(self, jumlah):
         if jumlah > 0:
             self.saldo += jumlah
+
+            query = "UPDATE rekening SET saldo = %s WHERE nomor_rekening = %s"
+            self.cursor.execute(query, (self.saldo, self.nomor_rekening))
+            self.conn.commit()
+
             print(f"Berhasil setor Rp {jumlah}. Saldo baru: Rp {self.saldo}")
+
         else:
             print("Jumlah setor harus lebih dari 0")
 
@@ -23,16 +40,27 @@ class Rekening:
             print("Saldo tidak mencukupi")
         else:
             self.saldo -= jumlah
-            print(f"Berhasil tarik Rp {jumlah}. Saldo baru: Rp {self.saldo}")
 
+        query = "UPDATE rekening SET saldo = %s WHERE nomor_rekening = %s"
+        self.cursor.execute(query, (self.saldo, self.nomor_rekening))
+        self.conn.commit()
 
-# ---------------------
-# Contoh penggunaan
-# ---------------------
+        print(f"Berhasil tarik Rp {jumlah}. Saldo baru: Rp {self.saldo}")
 
-rekening1 = Rekening(12345, "angelino", 500000)
+# rekening1 = Rekening(637211)
 
-rekening1.cek_saldo()
-rekening1.setor(250000)
-rekening1.tarik(300000)
-rekening1.cek_saldo()
+# rekening1.cek_saldo()
+# rekening1.setor(500000)
+# # rekening1.tarik(300000)
+# rekening1.cek_saldo()
+
+# rekening2 = Rekening(657709)
+# rekening2.cek_saldo()
+# rekening2.setor(500000)
+# rekening2.cek_saldo()
+
+rekening3 = Rekening(915437)
+rekening3.cek_saldo()
+rekening3.setor(500000)
+rekening3.cek_saldo()
+
